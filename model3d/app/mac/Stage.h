78d7cb3babfbe82f5e832ab7863be258ab1644cc
魏裕群 <409438071@qq.com>
2014-04-20 16:33:29 +0800

    -

---
+++ model3d/app/mac/Stage.h
@@ -0,0 +1,28 @@
+//
+//  Stage.h
+//  model3d
+//
+//  Created by 魏裕群 on 14-4-17.
+//  Copyright (c) 2014年 魏裕群. All rights reserved.
+//
+
+#ifndef __model3d__Stage_mac__
+#define __model3d__Stage_mac__
+#include "Scene.h"
+#include <vector>
+class Stage{
+public:
+    Stage();
+    virtual ~Stage();
+    void init();
+    void addScene(Scene*scene);
+    int run(Scene*scene);
+    int run();
+    static Stage* sharedInstance();
+protected:
+    long m_lAnimationInterval;  //micro second
+    GLFWwindow *m_pWindow;
+    std::vector<Scene*> m_iScenes;
+    static Stage * sm_pSharedStage;
+};
+#endif /* defined(__model3d__Stage_mac__) */
